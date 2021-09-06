0f1a45e82b6c7c9574a07bd62f28615cd933470a
Aditya Khandelwal <adityakhandelwal4201@gmail.com>
2021-09-06 07:54:44 +0000

    Minor changes

--- task3/src/App.js
+++ task3/src/App.js
@@ -1,26 +1,22 @@
-import logo from './logo.svg';
+import Header from "./components/Header";
 import './assets/css/plugins/plugins.min.css';
 import './assets/css/vendor/vendor.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div class="preloader-activate preloader-active open_tm_preloader">
+        <div class="preloader-area-wrap">
+            <div class="spinner d-flex justify-content-center align-items-center h-100">
+                <div class="bounce1"></div>
+                <div class="bounce2"></div>
+                <div class="bounce3"></div>
+            </div>
+        </div>
     </div>
+    <Header/>
+    </>
   );
 }
 
