1d6d92f7728e3b8b7480a7f4bea668a5ca2f171c
Aditya Khandelwal <adityakhandelwal4201@gmail.com>
2021-09-06 08:16:27 +0000

    index.html completed

--- task3/src/App.js
+++ task3/src/App.js
@@ -1,21 +1,10 @@
-import Header from "./components/Header";
-import './assets/css/plugins/plugins.min.css';
-import './assets/css/vendor/vendor.min.css';
-import './App.css';
+import Header from "./pages/Home";
+import "./App.css";
 
 function App() {
   return (
     <>
-    <div class="preloader-activate preloader-active open_tm_preloader">
-        <div class="preloader-area-wrap">
-            <div class="spinner d-flex justify-content-center align-items-center h-100">
-                <div class="bounce1"></div>
-                <div class="bounce2"></div>
-                <div class="bounce3"></div>
-            </div>
-        </div>
-    </div>
-    <Header/>
+      <Header />
     </>
   );
 }
